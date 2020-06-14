d6ecebaf33e186d1225c9b3d29f67fa5ae53fa6d
meier <meier@caritas-dicvhildesheim.de>
2020-06-14 11:36:58 +0200

    Initial commit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import bodyParser from "body-parser";
 import methodOverride from "method-override";
 import browserSync  from "browser-sync";
 import path from "path";
+import Dota from './lib/dota';
 
 // init logger
 const logger = createLogger({
@@ -64,3 +65,56 @@ if (ENV==="dev"){
 		},
 	});
 }
+
+
+let dotaClient = new Dota(process.env.STEAM_USERNAME, process.env.STEAM_PASSWORD)
+module.exports.dotaClient = dotaClient
+
+
+
+const intervalGetGamesAndRps = () => {
+    return dotaClient.requestRichPresence([...accounts]).then(rps => {
+        let lobby_ids = new Set()
+        let now = new Date()
+        if (!rps.length) return
+        rps.forEach(rp => rp.createdAt = now)
+        mongoDb.collection('rps').insertMany(rps)
+        rps.filter(rp => rp.WatchableGameID).forEach(rp => lobby_ids.add(rp.WatchableGameID))
+        return dotaClient.requestSourceTVGames({ start_game: 90, lobby_ids: [...lobby_ids] }).then(matches => {
+            let now = new Date()
+            matches = matches.map(match => {
+                let item = {
+                    average_mmr: match.average_mmr,
+                    game_mode: match.game_mode,
+                    league_id: match.league_id,
+                    match_id: match.match_id.toString(),
+                    lobby_id: match.lobby_id,
+                    lobby_type: match.lobby_type,
+                    players: match.players,
+                    server_steam_id: match.server_steam_id,
+                    weekend_tourney_bracket_round: match.weekend_tourney_bracket_round,
+                    weekend_tourney_skill_level: match.weekend_tourney_skill_level,
+                    createdAt: now
+                }
+                if (item.players) {
+                    item.players = item.players.map(player => ({
+                        account_id: player.account_id,
+                        hero_id: player.hero_id
+                    }))
+                }
+                return item
+            })
+            return 
+     
+    }).catch(() => { })
+})
+}
+
+
+let getGamesAndRpsInterval;
+getGamesAndRpsInterval = setInterval(intervalGetGamesAndRps, 30000)
+process.on('SIGTERM', () => {
+	clearInterval(getGamesAndRpsInterval)
+	dotaClient.exit()
+	process.exit(0)
+})
